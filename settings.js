a0f1722c24704ec1d4e9f93b08020f78c906a914
slroiya <107530719+slroiya@users.noreply.github.com>
2022-08-08 23:05:33 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -41,7 +41,7 @@ global.alivepic = `https://i.ibb.co/cyyCjN7/20220803-081426.png` // alive pictur
 global.dragonchat = "false"  // chat bot  "true" / "false" (200 auto replies in this)
 global.callblock = "false" // call block "true" / "false" ("true"  to block callers)
 global.antibad = "false" // To remove bad word senders
-global.antilink = "true" // To remove group link sharing peoples
+global.antilink = "false" // To remove group link sharing peoples
 global.upsongvoice = "true" // bot can upload song as voice note (it use your data and storage bacause auto download) you can put "false" (anyone can't get songs as voice note / your data is not wasted)
 
 global.owner = ['+94704343931']
